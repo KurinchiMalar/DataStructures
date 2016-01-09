97307cd297ae0dd8de02eae2940d2c88ecdd46f4
KurinchiMalar <kurinchimalar.n@gmail.com>
2016-01-09 10:57:12 +0530

    Adding more comments - Linear Selection logic of median finding.

--- Sorting/Median.py
+++ Sorting/Median.py
@@ -6,7 +6,7 @@
 # which can simply be
 #odd -> n/2
 #even -> (n/2) - 1
-from QuickSelect_kthsmallest import quick_select
+from kthLargestSmallest_QuickSelect import quick_select_kthsmallest
 from HeapSort import Heap
 ob = Heap()
 
@@ -22,7 +22,9 @@ def getMedian_SortingLogic(Ar):
     else:
         print "even"
         median = (len(Ar)/2)-1
-    print "Median element is :"+str(Ar[median])
+    #print "Median element is :"+str(Ar[median])
+
+    return Ar[median]
 
 
 #COMPLEXITY --> O(n) average , O(n*n) worstcase for QUICK_SELECT
@@ -30,17 +32,27 @@ def getMedian_LinearTime(Ar):
     if len(Ar) % 2 != 0:
         print "odd"
         median = len(Ar) / 2
+        print "median : "+str(median)
         print "Finding the : "+str(median+1)+"smallest..."
         #quick_select(Ar,0,len(Ar)-1,median+1)
     else:
         print "even"
         median = ( len(Ar) / 2 ) - 1
+        print "median : "+str(median)
         print "Finding the : "+str(median+1)+"smallest..."
+        # Array index starts from 0....so ideally...
+        # we now know position of the median element in this array...Find the medianpositionth smallest using quickselect
+
         #quick_select(Ar,0,len(Ar)-1,median+1)
-    return quick_select(Ar,0,len(Ar)-1,median+1)
+    return quick_select_kthsmallest(Ar,0,len(Ar)-1,median+1)
 
 Ar = [4,5,2,1,7,6,3,8,10,12]
+tempAr = Ar[:]
+Ar = [8,3,6,7,2,1,14,18,19,24,82]
+tempAr = Ar[:]
+#from Sorting.MergeSort import mergesort
+#print str(mergesort(Ar))
 
-#getMedian_SortingLogic(Ar)
+print "Median in sorting logic: "+str(getMedian_SortingLogic(tempAr))
 
-print str(getMedian_LinearTime(Ar))
+print "Median in Linear time: "+str(getMedian_LinearTime(Ar))
