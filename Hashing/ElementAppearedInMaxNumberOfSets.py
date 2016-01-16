e358e1d57cc87ff5552ec4b0bddded574c43366d
KurinchiMalar <kurinchimalar.n@gmail.com>
2016-01-16 12:02:23 +0530

    Element which appeared in maxnumber of sets - Adding the correct logic

--- Hashing/ElementAppearedInMaxNumberOfSets.py
+++ Hashing/ElementAppearedInMaxNumberOfSets.py
@@ -3,10 +3,11 @@
 
 '''
 # http://stackoverflow.com/questions/34809390/iterate-individual-elements-in-python-sets/34809464#34809464
+from _ast import List
 from collections import Counter
 from itertools import chain
 
-def find_element_which_appeared_in_max_sets_elegant(input_set):
+def find_element_which_appeared_in_maximum_overall_elegant(input_set):
 
     #print ""+str(Counter(chain.from_iterable(input_set)).most_common(1))
     #return Counter(chain.from_iterable(input_set)).most_common(1)
@@ -14,7 +15,7 @@ def find_element_which_appeared_in_max_sets_elegant(input_set):
 
 # Time Complexity : O(n)
 # Space Complexity : O(n)
-def find_element_which_appeared_in_max_sets(input_set):
+def find_element_which_appeared_maximum_overall(input_set):
 
     hash_table = {}
 
@@ -37,7 +38,84 @@ def find_element_which_appeared_in_max_sets(input_set):
 
     return max_occured_elem
 
+'''
+ Given m sets of integers that have n elements in them. Give an algorithm to find an element which appeared in maximum number of sets.
 
-input_set = {(5,4),(3,2),(4,3),(8,3)}
+ We have to return the element which appeared in maximum number of sets not maximum overall.
+
+ Algorithm:
+
+    For every element ->
+
+    maintain a hashentry like this -->
+
+        elem : [] # this list is used to put a 1 in indices of corresponding set_counter.
+
+                  # Just to indicate this element has occured in which set. If already set in set1(say) we can ignore. This is a duplicate.
+'''
+'''
+Space Complexity -
+
+say {(2,3,4,6),(5,5,5,2)}
+
+m = 2
+n = 4
+
+Hashtable : O(n)
+
+Then for every n, there is O(m) list
+
+n*O(m) = O(mn) = O(n)
+'''
+def create_new_list(set_counter, numberofsets):
+
+    temp = [0] * (numberofsets+1)
+    temp[set_counter] = 1
+    return temp
+
+def find_element_which_appeared_in_max_sets(input_set):
+    set_counter = 0
+    hash_table = {}
+    for set in input_set:
+        set_counter = set_counter + 1
+        for elem in set:
+            if elem not in hash_table:
+                hash_table[elem] = create_new_list(set_counter,input_set.__len__()) # if there are 3 sets, we need 0,1,2,3 indices
+            else:
+
+                existing_list = hash_table[elem]
+                if existing_list[set_counter] != 1:  # already set --> duplicate so ignore, set only if not set already.
+                    existing_list[set_counter] = 1
+                hash_table[elem] = existing_list
+
+    print hash_table
+
+    max_count = 0
+    max_occured_elem = -1
+    for elem in hash_table:
+
+        elem_list = hash_table[elem]
+        cur_list_sum = 0
+        for item in elem_list:
+            cur_list_sum += item
+        #print "elem:"+str(elem)+"...."+"cur_list_sum:"+str(cur_list_sum)
+        if cur_list_sum > max_count:
+            max_count = cur_list_sum
+            max_occured_elem = elem
+        #print "max_elem:"+str(max_occured_elem)+"...."+"max_count:"+str(max_count)
+
+    #print max_count
+    return max_occured_elem
+
+
+
+
+
+#input_set = {(5,4),(3,2),(4,3),(8,3)}
+
+input_set = {(5,5,5,2),(2,3,4,6)}
+#input_set = {(1,3,9,5),(4,4,4,2),(4,6,1,6),(1,7,7,9)}
 #print ""+str(find_element_which_appeared_in_max_sets(input_set))
-print ""+str(find_element_which_appeared_in_max_sets_elegant(input_set))
+#print ""+str(find_element_which_appeared_in_maximum_overall_elegant(input_set))
+
+print ""+str(find_element_which_appeared_in_max_sets(input_set))
