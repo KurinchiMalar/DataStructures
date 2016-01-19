3f69a9bb8b2f2971ecbabbf81e7388d00e6cc27d
KurinchiMalar <kurinchimalar.n@gmail.com>
2016-01-19 15:44:34 +0530

    Find the longest common substring - O(m*n) - Added another variation

--- Strings/LongestCommonSubstring.py
+++ Strings/LongestCommonSubstring.py
@@ -1,4 +1,7 @@
-
+'''
+Longest common substring of "GeeksforGeeks" and "GeeksQuiz" is "Geeks" of length 5.
+'''
+import sys
 '''
     Algorithm : construct lcs matrix...fill all entries 0 to m+1 and 0 to n+1 to 0.
 
@@ -42,11 +45,37 @@ def get_longest_common_substring(s1,s2):
     #print lcs
     return longest,result
 
+def get_smallest_common_substring(s1,s2):
 
+    i = 0
+    j = 0
+    m = len(s1)
+    n = len(s2)
 
+    lcs = [[0]*(n+1) for x in range(m+1)]
 
+    smallest = sys.maxint
+    result = set()
 
+    for i in range(0,m):
+        for j in range(0,n):
 
+            if s1[i] == s2[j]:
+
+                lcs[i+1][j+1] = 1 + lcs[i][j]   # Upto now longest will be the previous diagonal element.
+
+                if smallest > lcs[i+1][j+1]:
+
+                    result = set()
+                    smallest = lcs[i+1][j+1]
+
+                    result.add("".join(s1[i-smallest+1 : i+1]))
+
+                elif smallest == lcs[i+1][j+1]: # equal ==> longest substrings of equal length
+
+                    result.add("".join(s1[i-smallest+1 : i+1]))
+    #print lcs
+    return smallest,result
 
 s1 = "banana"
 s2 = "ana"
@@ -57,7 +86,40 @@ s2 = "abcdaba"
 #s1 = "ababcbcbaaabbdef"
 #s2 = "ababcbcbaaabbdefggggg"
 
+print "s1: "+str(s1)+"   s2: "+str(s2)
 length_of_longest_substring,result = get_longest_common_substring(list(s1),list(s2))
 
 print "Length : "+str(length_of_longest_substring)
 print "Longest Common Substring is :"+str(result)
+
+
+length_of_smallest_substring,result = get_smallest_common_substring(list(s1),list(s2))
+
+print "Length : "+str(length_of_smallest_substring)
+print "Smallest Common Substring is :"+str(result)
+
+
+def sameproblem_just_diff_logic_iandj(s1,s2):
+
+    m = len(s1)
+    n = len(s2)
+    lcs = [[0]*(n+1) for x in range(0,m+1)]
+    longest = -1
+    result = set()
+
+    for i in range(0,m+1):
+        for j in range(0,n+1):
+
+            if i == 0 or j == 0:
+                lcs[i][j] = 0
+
+            elif s1[i-1] == s2[j-1]:
+                lcs[i][j] = 1 + lcs[i-1][j-1]
+                if longest < lcs[i][j]:
+                    longest = lcs[i][j]
+                    result = set()
+                    result.add("".join(s1[i-longest:i]))
+                elif longest == lcs[i][j]:
+                    result.add("".join(s1[i-longest:i]))
+    print lcs
+    return longest,result
