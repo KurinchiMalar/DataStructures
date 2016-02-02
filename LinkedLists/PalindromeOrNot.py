b752aea2b8b32a9dcf60c7bd70bc3a687e953e1b
KurinchiMalar <kurinchimalar.n@gmail.com>
2016-02-02 15:51:31 +0530

    Check whether palindrome or not - 1) break 2) reverse second half 3) compare 4) fix it back - O(n)

--- LinkedLists/PalindromeOrNot.py
+++ LinkedLists/PalindromeOrNot.py
@@ -27,11 +27,32 @@ def get_middle_node(node):
         tort = tort.get_next()
         hare = hare.get_next().get_next()
 
+    # Let's return middle node, start of next list
+    # for odd list tort will be the middle node.
     if hare != None: # odd list.
-        return tort
-    else:
-        return prev_tort
+        return tort,tort.get_next()
+    else: # for even list prev_tort will be the middle node.
+        return prev_tort,tort
 
+def compare_lists(list1,list2):
+
+    temp1 = list1
+    temp2 = list2
+
+    while temp1 != None and temp2 != None:
+
+        if temp1.get_data() != temp2.get_data():
+            return 0
+        temp1 = temp1.get_next()
+        temp2 = temp2.get_next()
+
+    if temp1 == None and temp2 == None:
+        return 1
+    return 0
+
+
+# Time Complexity : O(n)
+# Space Complexity : O(1)
 def chec_pali(node):
 
     if node == None:
@@ -40,15 +61,40 @@ def chec_pali(node):
     if node.get_next() == None:
         return True
 
-    middlenode = get_middle_node(node)
+    prev_tort = node
+    tort = node
+    hare = node
 
-    traverse_list(middlenode)
-    print "middlenode: "+ListNode.ListNode.__str__(middlenode)
-    #revlist = reverse_recursive(middlenode)
+    while hare != None and hare.get_next() != None:
+        prev_tort = tort
+        tort = tort.get_next()
+        hare = hare.get_next().get_next()
 
 
-    #traverse_list(revlist)
+    if hare != None: # for odd list tort will be the middle node. so secondhalf starting will be tort.get_next()
+        middle_node = tort
+        tort = tort.get_next()
+    else: # for even list prev_tort will be the middle node.  so secondhalf starting will be tort.
+        middle_node = None
 
+    prev_tort.set_next(None)  # breaking firsthalf
+
+    tort = reverse_recursive(tort) # reversing second half
+
+    print "Comparing .... "
+    traverse_list(node)
+    traverse_list(tort)
+
+    result = compare_lists(node,tort) # comparing
+
+    if middle_node != None: # resetting odd list.
+        prev_tort.set_next(middle_node)
+        middle_node.set_next(reverse_recursive(tort))
+    else:
+        prev_tort.set_next(reverse_recursive(tort))
+
+    #traverse_list(node)
+    return result
 
 def traverse_list(node):
     current = node
@@ -62,11 +108,11 @@ def traverse_list(node):
 head = ListNode.ListNode(1)
 #print ListNode.ListNode.__str__(head)
 n1 = ListNode.ListNode(2)
-n2 = ListNode.ListNode(3)
+n2 = ListNode.ListNode(7)
 n3 = ListNode.ListNode(4)
-n4 = ListNode.ListNode(5)
-n5 = ListNode.ListNode(6)
-n6 = ListNode.ListNode(7)
+n4 = ListNode.ListNode(3)
+n5 = ListNode.ListNode(2)
+n6 = ListNode.ListNode(1)
 
 #orig_head = ListNode.ListNode(1)
 #orig_head.set_next(n1)
@@ -79,5 +125,5 @@ n4.set_next(n5)
 n5.set_next(n6)
 
 traverse_list(head)
-head1 = reverse_recursive(head)
-chec_pali(head1)
+#head1 = reverse_recursive(head)
+print "isPalindrome: "+str(chec_pali(head))
