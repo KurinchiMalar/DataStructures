219c1786eebaa3557fd91534b0f7252bad0f002b
KurinchiMalar <kurinchimalar.n@gmail.com>
2016-02-23 21:17:02 +0530

    Write a function to generate all possible n pairs of balanced parentheses. - O(n)

---
+++ Strings/ParanthesisCombinations.py
@@ -0,0 +1,53 @@
+'''
+Write a function to generate all possible n pairs of balanced parentheses.
+
+For example, if n=1
+{}
+for n=2
+{}{}
+{{}}
+'''
+# Time Complexity : O(n)
+# Space Complexity : O(1)
+
+def print_all_combinations_paranthesis(result,n,open,close):
+
+    if close == n:
+        print result
+
+        return
+
+    else:
+        if close < open:
+            #result.append(")")
+            print_all_combinations_paranthesis(result+")",n,open,close+1)
+
+        if open < n :
+            #result.append("(")
+            print_all_combinations_paranthesis(result+"(",n,open+1,close)
+
+def print_all_combinations_paranthesis_m2(source,pos,n,open,close):
+
+    if close == n:
+        source = "".join(source)
+        print source
+        return
+
+    else:
+        if close < open:
+            source[pos] = ")"
+            print_all_combinations_paranthesis_m2(source,pos+1,n,open,close+1)
+
+        if open < n :
+            #result.append("(")
+            source[pos] = "("
+            print_all_combinations_paranthesis_m2(source,pos+1,n,open+1,close)
+def invoke(n):
+    source = [None]* (n*2)
+    print "Method1 - without pos tracking"
+    print_all_combinations_paranthesis("",n,0,0)
+    print "Method2 - with pos tracking"
+    print_all_combinations_paranthesis_m2(source,0,n,0,0)
+
+
+invoke(3)
