2c5f842277b2b26261fccecadd22793672095300
KurinchiMalar <kurinchimalar.n@gmail.com>
2016-06-12 09:37:48 +0530

    Rotated Sorted Array - Find Elem - Correct code

--- Searching/RotatedSortedArray.py
+++ Searching/RotatedSortedArray.py
@@ -1,44 +1,58 @@
-'''
-     Given a sorted array of n integers that has been rotated unknown number of times. Give a O(logn) algorithm that finds
-     an element in the array.
-'''
-
-# Binary Search Method
-
-# Time Complexity : O(log n)
-
-def find_element_in_rotated_sorted_array(Ar,low,high,k):
-
-    print "----------------------------------"
-
-    print "(low , high):"+str(low)+","+str(high)
-    if low == high:
-        if Ar[low] == k:
-            return low
-
-    if low+1 == high:
-
-        if Ar[low] == k:
-            return low
-        elif Ar[high] == k:
-            return high
 
 
-    while low < high:
+def find_elem_rotated_sorted_array(Ar,first,last,k):
 
-        middle = (low+high) // 2
-        print "middle:"+str(middle)
-
-        if Ar[middle] == k:
-            return middle
-
-        if Ar[low] <= k and Ar[middle - 1] >= k:
-            return find_element_in_rotated_sorted_array(Ar,low,middle-1,k)
+    if first == last:
+        if Ar[first] == k:
+            return first
         else:
-            return find_element_in_rotated_sorted_array(Ar,middle+1,high,k)
+            return -1
 
+    if first+1 == last:
+        if Ar[first] == k:
+            return first
+        elif Ar[last] == k:
+            return last
+        else:
+            return -1
 
+    middle = (first + last) / 2
 
-Ar = [15,16,19,20,25,13,4,5,7,10,14]
+    if k == Ar[middle]:
+        return middle
+    #10 13 1 2 3 4 8 9
 
-print ""+str(find_element_in_rotated_sorted_array(Ar,0,len(Ar)-1,19))
+    #13
+    if k > Ar[middle] and k >= Ar[first] and Ar[middle] <= Ar[first]:
+        return find_elem_rotated_sorted_array(Ar, first, middle - 1, k)
+
+    #1
+    if k < Ar[middle] and k <= Ar[first] and Ar[middle] <= Ar[first]:
+        return find_elem_rotated_sorted_array(Ar, first, middle - 1, k)
+
+    #8
+    if k > Ar[middle] and k <= Ar[last] and Ar[middle] <= Ar[last]:  # 8
+        return find_elem_rotated_sorted_array(Ar, middle + 1, last, k)
+
+    # 4 8 9 10 13 1 2 3
+
+    #9
+    if k < Ar[middle] and k >= Ar[first] and Ar[middle] >= Ar[first]:
+        return find_elem_rotated_sorted_array(Ar, first, middle - 1, k)
+
+    #13
+    if k > Ar[middle] and k >= Ar[last] and Ar[middle] >= Ar[last]:  # 13
+        return find_elem_rotated_sorted_array(Ar, middle + 1, last, k)
+
+    #1
+    if k < Ar[middle] and k <= Ar[last] and Ar[middle] >= Ar[last]:
+        return find_elem_rotated_sorted_array(Ar, middle + 1, last, k)
+
+Ar = [10,13,1,2,3,4,8,9 ]
+for item in Ar:
+    print str(item) + " found at index "+str(find_elem_rotated_sorted_array(Ar,0,len(Ar)-1,item))
+
+Ar = [ 4,8,9,10,13,1,2,3 ]
+
+for item in Ar:
+    print str(item) + " found at index "+str(find_elem_rotated_sorted_array(Ar,0,len(Ar)-1,item))
